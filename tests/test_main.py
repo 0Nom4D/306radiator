13f34faeb5eb3d75788f96ade925df997632b7a6
Nom4D <arthur.adam@epitech.eu>
2021-11-23 14:18:33 +0100

    306radiator - Adding Main package unit tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,5 +6,19 @@
 ##
 
 from sources.main import printUsage, main
+import pytest
 
+
+class TestMain:
+
+    usageResult = "USAGE\n\t./306radiator n ir jr [i j]\n\nDESCRIPTION\n\tn\t\tsize of the room\n\
+\t(ir, jr)\tcoordinates of the radiator\n\t(i, j)\t\tcoordinates of a point in the room\n"
+
+    def test_printUsage(self):
+        assert not printUsage()
+
+    def test_printUsagePrints(self, capsys):
+        printUsage()
+        stdout = capsys.readouterr()[0]
+        assert stdout == self.usageResult
 
